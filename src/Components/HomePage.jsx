e9189c0abd39765beaa1c09792ea70d7eea0dff2
John-Andrew Registos <registosandrew@gmail.com>
2023-10-20 08:25:18 +0800

    video in assets folder

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Header from "./Header";
+import videoBg from "../assets/SHOWREEL.mp4";
 
 function HomePage() {
   return (
     <div id="home" className="w-full h-screen bg-black flex flex-col">
       <Header />
       <video
+        src={videoBg}
         autoPlay
         loop
         muted
         className="w-full h-full object-fill md:object-cover "
-      >
-        <source src="https://github.com/johnandrewregistos/johnandrew/blob/main/public/videos/showreel.mp4" />
-      </video>
+      ></video>
     </div>
   );
 }
